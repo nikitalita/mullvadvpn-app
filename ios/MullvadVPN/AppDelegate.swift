bd3fd3003b4ebae11b71b4f4f7f2b7388373bf1e
Andrej Mihajlov <and@mullvad.net>
2021-04-07 12:36:49 +0200

    AppDelegate: implement delegate methods and use UISplitViewController on iPad

--- ios/MullvadVPN/AppDelegate.swift
+++ ios/MullvadVPN/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var rootContainer: RootContainerViewController?
 
+    var logger: Logger?
+
     #if targetEnvironment(simulator)
     let simulatorTunnelProvider = SimulatorTunnelProviderHost()
     #endif
@@ -24,9 +26,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private let packetTunnelLogForwarder = LogStreamer<UTF8>(fileURLs: [ApplicationConfiguration.packetTunnelLogFileURL!])
     #endif
 
+    private weak var presentedSelectLocationViewController: SelectLocationViewController?
+
+    private var cachedRelays: CachedRelays? {
+        didSet {
+            if let cachedRelays = cachedRelays {
+                self.presentedSelectLocationViewController?.setCachedRelays(cachedRelays)
+            }
+        }
+    }
+    private var relayConstraints: RelayConstraints?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Setup logging
         initLoggingSystem(bundleIdentifier: Bundle.main.bundleIdentifier!)
+
+        self.logger = Logger(label: "AppDelegate")
 
         #if DEBUG
         let stdoutStream = TextFileOutputStream.standardOutputStream()
@@ -49,7 +64,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.rootViewController = launchController
 
         // Update relays
+        RelayCache.shared.addObserver(self)
         RelayCache.shared.updateRelays()
+
+        // Load initial relays
+        RelayCache.shared.read { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let cachedRelays):
+                    self.cachedRelays = cachedRelays
+
+                case .failure(let error):
+                    self.logger?.error(chainedError: error, message: "Failed to fetch initial relays")
+                }
+            }
+        }
 
         // Load tunnels
         TunnelManager.shared.loadTunnel(accountToken: Account.shared.token) { (result) in
@@ -58,19 +87,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     fatalError(error.displayChain(message: "Failed to load the tunnel for account"))
                 }
 
-                self.rootContainer = RootContainerViewController()
-                self.rootContainer?.delegate = self
-                self.window?.rootViewController = self.rootContainer
+                TunnelManager.shared.getRelayConstraints { (result) in
+                    DispatchQueue.main.async {
+                        switch result {
+                        case .success(let relayConstraints):
+                            self.relayConstraints = relayConstraints
 
-                switch UIDevice.current.userInterfaceIdiom {
-                case .pad:
-                    self.startPadInterfaceFlow()
+                        case .failure(let error):
+                            self.logger?.error(chainedError: error, message: "Failed to load relay constraints")
+                        }
 
-                case .phone:
-                    self.startPhoneInterfaceFlow()
+                        self.rootContainer = RootContainerViewController()
+                        self.rootContainer?.delegate = self
+                        self.window?.rootViewController = self.rootContainer
 
-                default:
-                    fatalError()
+                        switch UIDevice.current.userInterfaceIdiom {
+                        case .pad:
+                            self.setupPadUI()
+
+                        case .phone:
+                            self.setupPhoneUI()
+
+                        default:
+                            fatalError()
+                        }
+                    }
                 }
             }
         }
@@ -87,8 +128,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         TunnelManager.shared.refreshTunnelState(completionHandler: nil)
     }
 
-    private func startPadInterfaceFlow() {
-        self.rootContainer?.setViewControllers([ConnectViewController()], animated: false)
+    private func setupPadUI() {
+        let selectLocationController = makeSelectLocationController()
+        let connectController = makeConnectViewController()
+
+        let splitViewController = UISplitViewController()
+        splitViewController.minimumPrimaryColumnWidth = 300
+        splitViewController.preferredPrimaryColumnWidthFraction = 0.3
+        splitViewController.primaryEdge = .trailing
+        splitViewController.preferredDisplayMode = .allVisible
+        splitViewController.viewControllers = [selectLocationController, connectController]
+
+        self.rootContainer?.setViewControllers([splitViewController], animated: false)
+        self.presentedSelectLocationViewController = selectLocationController
 
         if !Account.shared.isAgreedToTermsOfService {
             let consentViewController = self.makeConsentController { [weak self] (viewController) in
@@ -107,10 +159,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.rootContainer?.present(consentViewController, animated: true)
         } else if !Account.shared.isLoggedIn {
             self.rootContainer?.present(makeLoginController(), animated: true)
+        } else {
+            self.showAccountSettingsControllerIfAccountExpired()
         }
     }
 
-    private func startPhoneInterfaceFlow() {
+    private func setupPhoneUI() {
         let showNextController = { [weak self] (_ animated: Bool) in
             guard let self = self else { return }
 
@@ -118,7 +172,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             var viewControllers: [UIViewController] = [loginViewController]
 
             if Account.shared.isLoggedIn {
-                viewControllers.append(ConnectViewController())
+                viewControllers.append(self.makeConnectViewController())
             }
 
             self.rootContainer?.setViewControllers(viewControllers, animated: animated) {
@@ -135,6 +189,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
             self.rootContainer?.setViewControllers([consentViewController], animated: false)
         }
+    }
+
+    private func makeConnectViewController() -> ConnectViewController {
+        let connectController = ConnectViewController()
+        connectController.delegate = self
+
+        return connectController
+    }
+
+    private func makeSelectLocationController() -> SelectLocationViewController {
+        let selectLocationController = SelectLocationViewController()
+        selectLocationController.delegate = self
+
+        if let cachedRelays = cachedRelays {
+            selectLocationController.setCachedRelays(cachedRelays)
+        }
+
+        if let relayLocation = relayConstraints?.location.value {
+            selectLocationController.setSelectedRelayLocation(relayLocation, animated: false, scrollPosition: .middle)
+        }
+
+        return selectLocationController
     }
 
     private func makeConsentController(completion: @escaping (UIViewController) -> Void) -> ConsentViewController {
@@ -206,6 +282,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 }
 
 extension AppDelegate: RootContainerViewControllerDelegate {
+    func rootContainer(_ controller: RootContainerViewController, preferredWidthForDetailViewWithContainerSize containerSize: CGSize) -> CGFloat {
+        return max(300, containerSize.width * 0.3)
+    }
 
     func rootContainerViewControllerShouldShowSettings(_ controller: RootContainerViewController, navigateTo route: SettingsNavigationRoute?, animated: Bool) {
         let navController = makeSettingsNavigationController(route: route)
@@ -227,18 +306,65 @@ extension AppDelegate: RootContainerViewControllerDelegate {
 
 extension AppDelegate: LoginViewControllerDelegate {
 
+    func loginViewController(_ controller: LoginViewController, loginWithAccountToken accountToken: String, completion: @escaping (Result<AccountResponse, Account.Error>) -> Void) {
+        Account.shared.login(with: accountToken) { (result) in
+            switch result {
+            case .success:
+                self.logger?.debug("Logged in with existing token")
+
+            case .failure(let error):
+                self.logger?.error(chainedError: error, message: "Failed to log in with existing account")
+            }
+
+            completion(result)
+        }
+    }
+
+    func loginViewControllerLoginWithNewAccount(_ controller: LoginViewController, completion: @escaping (Result<AccountResponse, Account.Error>) -> Void) {
+        Account.shared.loginWithNewAccount { (result) in
+            switch result {
+            case .success:
+                self.logger?.debug("Logged in with new account token")
+
+            case .failure(let error):
+                self.logger?.error(chainedError: error, message: "Failed to log in with new account")
+            }
+
+            completion(result)
+        }
+    }
+
     func loginViewControllerDidLogin(_ controller: LoginViewController) {
-        switch UIDevice.current.userInterfaceIdiom {
-        case .phone:
-            rootContainer?.pushViewController(ConnectViewController(), animated: true) {
-                self.showAccountSettingsControllerIfAccountExpired()
+        self.window?.isUserInteractionEnabled = false
+
+        TunnelManager.shared.getRelayConstraints { [weak self] (result) in
+            guard let self = self else { return }
+
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let relayConstraints):
+                    self.relayConstraints = relayConstraints
+                    self.presentedSelectLocationViewController?.setSelectedRelayLocation(relayConstraints.location.value, animated: false, scrollPosition: .middle)
+
+                case .failure(let error):
+                    self.logger?.error(chainedError: error, message: "Failed to load relay constraints after log in")
+                }
+
+                switch UIDevice.current.userInterfaceIdiom {
+                case .phone:
+                    self.rootContainer?.pushViewController(self.makeConnectViewController(), animated: true) {
+                        self.showAccountSettingsControllerIfAccountExpired()
+                    }
+                case .pad:
+                    controller.dismiss(animated: true) {
+                        self.showAccountSettingsControllerIfAccountExpired()
+                    }
+                default:
+                    fatalError()
+                }
+
+                self.window?.isUserInteractionEnabled = true
             }
-        case .pad:
-            controller.dismiss(animated: true) {
-                self.showAccountSettingsControllerIfAccountExpired()
-            }
-        default:
-            fatalError()
         }
     }
 
@@ -273,6 +399,120 @@ extension AppDelegate: SettingsNavigationControllerDelegate {
 
 }
 
+
+extension AppDelegate: ConnectViewControllerDelegate {
+
+    func connectViewControllerShouldShowSelectLocationPicker(_ controller: ConnectViewController) {
+        let contentController = makeSelectLocationController()
+        contentController.navigationItem.title = NSLocalizedString("Select location", comment: "Navigation title")
+        contentController.navigationItem.largeTitleDisplayMode = .never
+        contentController.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(handleDismissSelectLocationController(_:)))
+
+        let navController = SelectLocationNavigationController(contentController: contentController)
+        self.rootContainer?.present(navController, animated: true)
+        self.presentedSelectLocationViewController = contentController
+    }
+
+    func connectViewControllerShouldConnectTunnel(_ controller: ConnectViewController, completion: @escaping (TunnelManager.Error?) -> Void) {
+        connectTunnel(completion: completion)
+    }
+
+    func connectViewControllerShouldDisconnectTunnel(_ controller: ConnectViewController, completion: @escaping (TunnelManager.Error?) -> Void) {
+        disconnectTunnel(completion: completion)
+    }
+
+    func connectViewControllerShouldReconnectTunnel(_ controller: ConnectViewController) {
+        TunnelManager.shared.reconnectTunnel(completionHandler: nil)
+    }
+
+    @objc private func handleDismissSelectLocationController(_ sender: Any) {
+        self.presentedSelectLocationViewController?.dismiss(animated: true)
+    }
+
+    private func connectTunnel(completion: @escaping (TunnelManager.Error?) -> Void) {
+        TunnelManager.shared.startTunnel { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .success:
+                    completion(nil)
+
+                case .failure(let error):
+                    self.logger?.error(chainedError: error, message: "Failed to start the VPN tunnel")
+                    completion(error)
+                }
+            }
+        }
+    }
+
+    private func disconnectTunnel(completion: @escaping (TunnelManager.Error?) -> Void) {
+        TunnelManager.shared.stopTunnel { (result) in
+            switch result {
+            case .success:
+                completion(nil)
+
+            case .failure(let error):
+                self.logger?.error(chainedError: error, message: "Failed to stop the VPN tunnel")
+
+                completion(error)
+            }
+        }
+    }
+}
+
+extension AppDelegate: SelectLocationViewControllerDelegate {
+    func selectLocationViewController(_ controller: SelectLocationViewController, didSelectRelayLocation relayLocation: RelayLocation) {
+        switch UIDevice.current.userInterfaceIdiom  {
+        case .phone:
+            self.window?.isUserInteractionEnabled = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(250)) {
+                self.window?.isUserInteractionEnabled = true
+                controller.dismiss(animated: true) {
+                    self.selectLocationControllerDidSelectRelayLocation(relayLocation)
+                }
+            }
+
+        case .pad:
+            selectLocationControllerDidSelectRelayLocation(relayLocation)
+
+        default:
+            fatalError()
+        }
+    }
+
+    private func selectLocationControllerDidSelectRelayLocation(_ relayLocation: RelayLocation) {
+        let relayConstraints = RelayConstraints(location: .only(relayLocation))
+
+        TunnelManager.shared.setRelayConstraints(relayConstraints) { [weak self] (result) in
+            guard let self = self else { return }
+
+            DispatchQueue.main.async {
+                self.relayConstraints = relayConstraints
+
+                switch result {
+                case .success:
+                    self.logger?.debug("Updated relay constraints: \(relayConstraints)")
+                    self.connectTunnel { (error) in
+                        // TODO: show error?
+                    }
+
+                case .failure(let error):
+                    self.logger?.error(chainedError: error, message: "Failed to update relay constraints")
+                }
+            }
+        }
+    }
+}
+
+extension AppDelegate: RelayCacheObserver {
+
+    func relayCache(_ relayCache: RelayCache, didUpdateCachedRelays cachedRelays: CachedRelays) {
+        DispatchQueue.main.async {
+            self.cachedRelays = cachedRelays
+        }
+    }
+
+}
+
 extension AppDelegate: AppStorePaymentManagerDelegate {
 
     func appStorePaymentManager(_ manager: AppStorePaymentManager,
@@ -282,4 +522,24 @@ extension AppDelegate: AppStorePaymentManagerDelegate {
         // app launches, we assume that all successful purchases belong to the active account token.
         return Account.shared.token
     }
+
+}
+
+
+extension UISplitViewController: RootContainment {
+
+    var preferredHeaderBarStyle: HeaderBarStyle {
+        for case let viewController as RootContainment in viewControllers {
+            return viewController.preferredHeaderBarStyle
+        }
+        return .default
+    }
+
+    var prefersHeaderBarHidden: Bool {
+        for case let viewController as RootContainment in viewControllers {
+            return viewController.prefersHeaderBarHidden
+        }
+        return false
+    }
+
 }
